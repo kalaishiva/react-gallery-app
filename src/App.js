3c4dde3c2d7d71858b5b80ae6b2f7e763651a66b
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-10-26 09:45:35 +0200

    React tailwind configuration completed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './index.css';
+
+function App() {
+  return (
+    <div >
+     <h1 className="text-5xl font-bold text-blue-800	 underline">
+      React Gallery App
+    </h1>
+    
+    </div>
+  );
+}
+
+export default App;
